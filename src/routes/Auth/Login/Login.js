9ffbd125c6b6c5d10722d1685ab69b858ab188ee
Akanowo <ukoakanowo98@gmail.com>
2022-03-31 08:46:58 +0100

    added validation on auth forms

--- src/routes/Auth/Login/Login.js
+++ src/routes/Auth/Login/Login.js
@@ -4,20 +4,32 @@ import { toast } from 'react-toastify';
 import Loader from '../../../components/Loader/Loader';
 import { Link, Navigate as Redirect } from 'react-router-dom';
 import AddToHomeScreen from '../../../containers/AddToHomeScreen/AddToHomeScreen';
+import { useForm } from 'react-hook-form';
+import { CloseCircle, CloseCircleOutline } from 'react-ionicons';
 
 function Login() {
 	const [email, setEmail] = useState('');
 	const [password, setPasword] = useState('');
 	const [redirect, setRedirect] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm();
+
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 	const submitHandler = async (e) => {
-		e.preventDefault();
+		// e.preventDefault();
 		setIsLoading(true);
 		const data = {
 			email: email.trim().toLowerCase(),
 			password: password.trim(),
 		};
+
+		console.log(data);
 
 		let response;
 		try {
@@ -61,45 +73,63 @@ function Login() {
 									<h4>Fill the form to log in</h4>
 								</div>
 								<div className="section mb-5 p-2">
-									<form onSubmit={submitHandler}>
+									<form onSubmit={handleSubmit(submitHandler)}>
 										<div className="card">
 											<div className="card-body pb-1">
 												<div className="form-group basic">
 													<div className="input-wrapper">
-														<label className="label" htmlFor="email1">
+														<label className="label" htmlFor="email">
 															E-mail
 														</label>
 														<input
 															type="email"
 															className="form-control"
-															id="email1"
+															id="email"
 															placeholder="Your e-mail"
+															{...register('email', {
+																required: true,
+																pattern: emailRegex,
+															})}
 															value={email}
 															onChange={(e) => setEmail(e.target.value)}
 														/>
 														<i className="clear-input">
-															<ion-icon name="close-circle"></ion-icon>
+															<CloseCircle color={'#000'} />
 														</i>
+														<div className="input-errors">
+															{errors.email?.type === 'required' &&
+																'Email is required'}
+															{errors.email?.type === 'pattern' &&
+																'Invalid Email Addrress'}
+														</div>
 													</div>
 												</div>
 
 												<div className="form-group basic">
 													<div className="input-wrapper">
-														<label className="label" htmlFor="password1">
+														<label className="label" htmlFor="password">
 															Password
 														</label>
 														<input
 															type="password"
 															className="form-control"
-															id="password1"
+															id="password"
 															autoComplete="off"
 															placeholder="Your password"
+															{...register('password', {
+																required: true,
+																max: 16,
+															})}
 															value={password}
 															onChange={(e) => setPasword(e.target.value)}
 														/>
 														<i className="clear-input">
-															<ion-icon name="close-circle"></ion-icon>
+															<CloseCircleOutline color={'#000'} />
 														</i>
+														<div className="input-errors">
+															{errors.password?.type === 'required' &&
+																'Password is required'}
+														</div>
 													</div>
 												</div>
 											</div>
--- src/routes/Auth/Signup/Signup.js
+++ src/routes/Auth/Signup/Signup.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { CloseCircle, CloseCircleOutline } from 'react-ionicons';
+import { CloseCircle, CloseCircleOutline, Triangle } from 'react-ionicons';
 import axios from '../../../constants/axiosInstance';
 import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import Loader from '../../../components/Loader/Loader';
 
 function Signup() {
 	const [firstName, setFirstName] = useState('');
@@ -11,9 +13,20 @@ function Signup() {
 	const [repeat_pwd, setRepeatPwd] = useState('');
 	const [vendorName, setVendorName] = useState('');
 	const [type, setType] = useState('customer');
+	const [isLoading, setIsLoading] = useState(false);
+	const {
+		register,
+		formState: { errors },
+		handleSubmit: submit,
+	} = useForm();
+
+	const nameRegex = /^[a-zA-Z]*$/;
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const vendorNameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
 
 	const handleSubmit = async (e) => {
-		e.preventDefault();
+		// e.preventDefault();
 
 		if (password !== repeat_pwd) {
 			return toast("Passwords don't match", {
@@ -21,6 +34,8 @@ function Signup() {
 				position: 'top-center',
 			});
 		}
+
+		setIsLoading(true);
 
 		const data = {
 			firstName,
@@ -37,6 +52,9 @@ function Signup() {
 			response = await (await axios.post('/auth/signup', data)).data;
 		} catch (error) {
 			if (error.response) {
+				setIsLoading(false);
+				setPassword('');
+				setRepeatPwd('');
 				toast(error.response.data.error, {
 					position: 'top-center',
 					type: 'error',
@@ -49,6 +67,8 @@ function Signup() {
 				type: 'error',
 			});
 		}
+
+		setIsLoading(false);
 
 		if (!response) {
 			return toast('An error occured', {
@@ -75,193 +95,253 @@ function Signup() {
 
 	return (
 		<>
-			<div id="appCapsule">
-				<div className="section mt-2 text-center">
-					<h1>Register now</h1>
-					<h4>Create an account</h4>
-				</div>
-				<div className="section mb-5 p-2">
-					<form onSubmit={handleSubmit}>
-						<div className="card">
-							<div className="card-body">
-								<ul className="nav nav-tabs">
-									<li className="nav-item" onClick={() => setType('customer')}>
-										<a
-											className={`nav-link ${
-												type === 'customer' ? 'active' : ''
-											}`}
-											aria-current="page"
-											href="#"
-											style={{ color: '#000' }}
+			{isLoading ? (
+				<Loader />
+			) : (
+				<div id="appCapsule">
+					<div className="section mt-2 text-center">
+						<h1>Register now</h1>
+						<h4>Create an account</h4>
+					</div>
+					<div className="section mb-5 p-2">
+						<form onSubmit={submit(handleSubmit)}>
+							<div className="card">
+								<div className="card-body">
+									<ul className="nav nav-tabs">
+										<li
+											className="nav-item"
+											onClick={() => setType('customer')}
 										>
-											Customer
-										</a>
-									</li>
-									<li className="nav-item" onClick={() => setType('vendor')}>
-										<a
-											className={`nav-link ${
-												type === 'vendor' ? 'active' : ''
-											}`}
-											aria-current="page"
-											href="#"
-											style={{ color: '#000' }}
-										>
-											Vendor
-										</a>
-									</li>
-								</ul>
+											<a
+												className={`nav-link ${
+													type === 'customer' ? 'active' : ''
+												}`}
+												aria-current="page"
+												href="#"
+												style={{ color: '#000' }}
+											>
+												Customer
+											</a>
+										</li>
+										<li className="nav-item" onClick={() => setType('vendor')}>
+											<a
+												className={`nav-link ${
+													type === 'vendor' ? 'active' : ''
+												}`}
+												aria-current="page"
+												href="#"
+												style={{ color: '#000' }}
+											>
+												Vendor
+											</a>
+										</li>
+									</ul>
 
-								{type === 'vendor' ? (
+									{type === 'vendor' ? (
+										<div className="form-group basic">
+											<div className="input-wrapper">
+												<label className="label" htmlFor="vendorName">
+													Vendor Name
+												</label>
+												<input
+													type="text"
+													className="form-control"
+													id="vendorName"
+													placeholder="Enter Vendor Name"
+													{...register('vendorName', {
+														required: true,
+														pattern: vendorNameRegex,
+													})}
+													value={vendorName}
+													onChange={(e) => setVendorName(e.target.value)}
+												/>
+												<i className="clear-input">
+													<CloseCircle />
+												</i>
+												<div className="input-errors">
+													{errors.vendorName?.type === 'required' &&
+														'Vendor name is required'}
+													{errors.vendorName?.type === 'pattern' &&
+														'Invalid vendor name'}
+												</div>
+											</div>
+										</div>
+									) : null}
+
 									<div className="form-group basic">
 										<div className="input-wrapper">
-											<label className="label" htmlFor="vendorName">
-												Vendor Name
+											<label className="label" htmlFor="firstname">
+												Firstname
 											</label>
 											<input
+												autoComplete="off"
 												type="text"
 												className="form-control"
-												id="vendorName"
-												placeholder="Enter Vendor Name"
-												value={vendorName}
-												onChange={(e) => setVendorName(e.target.value)}
+												id="firstname"
+												placeholder="Enter Firstname"
+												{...register('firstname', {
+													required: true,
+													pattern: nameRegex,
+												})}
+												value={firstName}
+												onChange={(e) => setFirstName(e.target.value)}
+											/>
+											<i className="clear-input">
+												<CloseCircleOutline color={'#000'} />
+											</i>
+											<div className="input-errors">
+												{errors.firstname?.type === 'required' &&
+													'Firstname is required'}
+												{errors.firstname?.type === 'pattern' &&
+													'Invalid firstname'}
+											</div>
+										</div>
+									</div>
+
+									<div className="form-group basic">
+										<div className="input-wrapper">
+											<label className="label" htmlFor="lastname">
+												Lastname
+											</label>
+											<input
+												autoComplete="off"
+												type="text"
+												className="form-control"
+												id="lastname"
+												placeholder="Enter Lastname"
+												{...register('lastname', {
+													required: true,
+													pattern: nameRegex,
+												})}
+												value={lastName}
+												onChange={(e) => setLastName(e.target.value)}
 											/>
 											<i className="clear-input">
 												<CloseCircle />
 											</i>
+											<div className="input-errors">
+												{errors.lastname?.type === 'required' &&
+													'Lastname is required'}
+												{errors.lastname?.type === 'pattern' &&
+													'Invalid lastname'}
+											</div>
 										</div>
 									</div>
-								) : null}
 
-								<div className="form-group basic">
-									<div className="input-wrapper">
-										<label className="label" htmlFor="firstname">
-											Firstname
-										</label>
-										<input
-											type="text"
-											className="form-control"
-											id="firstname"
-											placeholder="Enter Firstname"
-											value={firstName}
-											onChange={(e) => setFirstName(e.target.value)}
-										/>
-										<i className="clear-input">
-											<CloseCircleOutline color={'#000'} />
-										</i>
+									<div className="form-group basic">
+										<div className="input-wrapper">
+											<label className="label" htmlFor="email">
+												E-mail
+											</label>
+											<input
+												autoComplete="off"
+												type="email"
+												className="form-control"
+												id="email"
+												placeholder="Your e-mail"
+												{...register('email', {
+													required: true,
+													pattern: emailRegex,
+												})}
+												value={email}
+												onChange={(e) => setEmail(e.target.value)}
+											/>
+											<i className="clear-input">
+												<CloseCircle />
+											</i>
+											<div className="input-errors">
+												{errors.email?.type === 'required' &&
+													'Email is required'}
+												{errors.email?.type === 'pattern' &&
+													'Invalid email address'}
+											</div>
+										</div>
 									</div>
+
+									<div className="form-group basic">
+										<div className="input-wrapper">
+											<label className="label" htmlFor="password">
+												Password
+											</label>
+											<input
+												type="password"
+												className="form-control"
+												id="password"
+												autoComplete="off"
+												placeholder="Your password"
+												{...register('password', { required: true })}
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+											<i className="clear-input">
+												<CloseCircle />
+											</i>
+											<div className="input-errors">
+												{errors.password?.type === 'required' &&
+													'Password is required'}
+											</div>
+										</div>
+									</div>
+
+									<div className="form-group basic">
+										<div className="input-wrapper">
+											<label className="label" htmlFor="repeat_password">
+												Password Again
+											</label>
+											<input
+												type="password"
+												className="form-control"
+												id="repeat_password"
+												autoComplete="off"
+												placeholder="Type password again"
+												{...register('confirm_pwd', { required: true })}
+												value={repeat_pwd}
+												onChange={(e) => setRepeatPwd(e.target.value)}
+											/>
+											<i className="clear-input">
+												<CloseCircle />
+											</i>
+											<div className="input-errors">
+												{errors.confirm_pwd?.type === 'required' &&
+													'Please enter password again'}
+											</div>
+										</div>
+									</div>
+
+									{/* <div className="custom-control custom-checkbox mt-2 mb-1">
+								<div className="form-check">
+									<input
+										type="checkbox"
+										className="form-check-input"
+										id="customCheckb1"
+									/>
+									<label className="form-check-label" htmlFor="customCheckb1">
+										I agree{' '}
+										<a
+											href="#"
+											data-bs-toggle="modal"
+											data-bs-target="#termsModal"
+										>
+											terms and conditions
+										</a>
+									</label>
 								</div>
-
-								<div className="form-group basic">
-									<div className="input-wrapper">
-										<label className="label" htmlFor="lastname">
-											Lastname
-										</label>
-										<input
-											type="text"
-											className="form-control"
-											id="lastname"
-											placeholder="Enter Lastname"
-											value={lastName}
-											onChange={(e) => setLastName(e.target.value)}
-										/>
-										<i className="clear-input">
-											<CloseCircle />
-										</i>
-									</div>
+							</div> */}
 								</div>
-
-								<div className="form-group basic">
-									<div className="input-wrapper">
-										<label className="label" htmlFor="email">
-											E-mail
-										</label>
-										<input
-											type="email"
-											className="form-control"
-											id="email"
-											placeholder="Your e-mail"
-											value={email}
-											onChange={(e) => setEmail(e.target.value)}
-										/>
-										<i className="clear-input">
-											<CloseCircle />
-										</i>
-									</div>
-								</div>
-
-								<div className="form-group basic">
-									<div className="input-wrapper">
-										<label className="label" htmlFor="password">
-											Password
-										</label>
-										<input
-											type="password"
-											className="form-control"
-											id="password"
-											autoComplete="off"
-											placeholder="Your password"
-											value={password}
-											onChange={(e) => setPassword(e.target.value)}
-										/>
-										<i className="clear-input">
-											<CloseCircle />
-										</i>
-									</div>
-								</div>
-
-								<div className="form-group basic">
-									<div className="input-wrapper">
-										<label className="label" htmlFor="repeat_password">
-											Password Again
-										</label>
-										<input
-											type="password"
-											className="form-control"
-											id="repeat_password"
-											autoComplete="off"
-											placeholder="Type password again"
-											value={repeat_pwd}
-											onChange={(e) => setRepeatPwd(e.target.value)}
-										/>
-										<i className="clear-input">
-											<CloseCircle />
-										</i>
-									</div>
-								</div>
-
-								{/* <div className="custom-control custom-checkbox mt-2 mb-1">
-									<div className="form-check">
-										<input
-											type="checkbox"
-											className="form-check-input"
-											id="customCheckb1"
-										/>
-										<label className="form-check-label" htmlFor="customCheckb1">
-											I agree{' '}
-											<a
-												href="#"
-												data-bs-toggle="modal"
-												data-bs-target="#termsModal"
-											>
-												terms and conditions
-											</a>
-										</label>
-									</div>
-								</div> */}
 							</div>
-						</div>
 
-						<div className="form-button-group transparent">
-							<button
-								type="submit"
-								className="btn btn-primary btn-block btn-lg"
-							>
-								Register
-							</button>
-						</div>
-					</form>
+							<div className="form-button-group transparent">
+								<button
+									type="submit"
+									className="btn btn-primary btn-block btn-lg"
+								>
+									Register
+								</button>
+							</div>
+						</form>
+					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 }
